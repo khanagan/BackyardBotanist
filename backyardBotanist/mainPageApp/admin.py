134c9e1db6e9f003976f6d39b188525f4bb51c5f
Kayleigh <kayleighrf@gmail.com>
2021-03-24 00:12:51 -0400

    Registered tables in admin.py

--- backyardBotanist/mainPageApp/admin.py
+++ backyardBotanist/mainPageApp/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Plant
+from .models import Plant, User, ConservationRank, Pictures, Group, Subgroup, Location
 admin.site.register(Plant)
+admin.site.register(User)
+admin.site.register(ConservationRank)
+admin.site.register(Group)
+admin.site.register(Subgroup)
+admin.site.register(Pictures)
+admin.site.register(Location)
+
