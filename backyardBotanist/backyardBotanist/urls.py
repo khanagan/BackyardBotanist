63db051fda10af9a2fce205875b8acb1c8a4b66f
Kayleigh <kayleighrf@gmail.com>
2021-03-24 01:43:08 -0400

    Homepage created with text, "Backyard Botanist". Webpage creation should now be functional.

--- backyardBotanist/backyardBotanist/urls.py
+++ backyardBotanist/backyardBotanist/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('mainPageApp/', include('mainPageApp.appUrls')),
+    path('', include('mainPageApp.appUrls')),
     path('admin/', admin.site.urls)
 ]
--- backyardBotanist/mainPageApp/views.py
+++ backyardBotanist/mainPageApp/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 # Create your views here.
 
-def index(request):
-    return  HttpResponse("Basic Response Check")
+def home(request):
+    page = loader.get_template('home.html')
+    return HttpResponse(page.render())
