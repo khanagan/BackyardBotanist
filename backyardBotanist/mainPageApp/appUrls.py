cfde97fba532dc649155e53604d3a7f4b0e350fe
Kayleigh <kayleighrf@gmail.com>
2021-03-26 03:34:20 -0400

    Updates to html pages. Page to page navigation now possible. A modified settings.py will be in the next commit.

--- backyardBotanist/mainPageApp/appUrls.py
+++ backyardBotanist/mainPageApp/appUrls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('changePassword', views.changePassword, name='changePassword'),
+    path('invalidUser', views.invalidUser, name='invalidUser'),
 ]
--- backyardBotanist/mainPageApp/views.py
+++ backyardBotanist/mainPageApp/views.py
@@ -1,8 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import User
 # Create your views here.
 
 def home(request):
     page = loader.get_template('home.html')
     return HttpResponse(page.render())
+
+def changePassword(request):
+    page = loader.get_template('changePassword.html')
+    return HttpResponse(page.render())
+
+def invalidUser(request):
+    if
+    page = loader.get_template('invalidUser.html')
+    return HttpResponse(page.render())
+
+
