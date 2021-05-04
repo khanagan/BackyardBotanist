24f6d34333af0bef855703a4c4ae4464fb155c2f
Kayleigh <kayleighrf@gmail.com>
2021-05-04 16:24:01 -0400

    Completed 2nd Report
    Updated code with 4 reports pages, 2nd report assignment complete.

--- backyardBotanist/backyardBotanist/settings.py
+++ backyardBotanist/backyardBotanist/settings.py
@@ -78,6 +78,15 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'plants',
+        'USER': 'tester_francis',
+        'PASSWORD': '153!4!',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'OPTIONS':{
+            'init_command':"SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
--- backyardBotanist/mainPageApp/views.py
+++ backyardBotanist/mainPageApp/views.py
@@ -3,7 +3,10 @@ from django.http import HttpResponse
 from django.template import loader
 from django.templatetags import static
 from django.shortcuts import get_object_or_404
-from .models import User
+from django.db import connection
+from .models import User, Plant
+
+
 # Create your views here.
 
 def home(request):
@@ -22,4 +25,23 @@ def databaseSearchPage(request):
     page = loader.get_template('databaseSearchPage.html')
     return HttpResponse(page.render())
 
+def displayReport1(request):
+    #plants=Plant.objects.all()
+    #plants = Plant.objects.all().values('plantId','commonName','scientificName','yearLastDocumented','rankId','groupId','groupId__taxGroup','subgroupId','statusId')
+    plants = Plant.objects.raw('SELECT 1 as id, plantId, commonName, scientificName, yearLastDocumented, rankID, groupID, subgroupID, statusID from Plant limit 10')
+    return render(request, "reportPage1.html", {"Plant": plants})
+
+def displayReport2(request):
+    plants=Plant.objects.all()
+    #plants = Plant.objects.all().values('plantId','commonName','scientificName','yearLastDocumented','rankId','groupId','groupId__taxGroup','subgroupId','statusId')
+    with connection.cursor() as cursor:
+        cursor.execute("SELECT PlantID, CommonName, ScientificName, YearLastDocumented, Plant.StatusID AS StatusID, FederalListingStatus, StateListingStatus FROM Plant, ListingStatus WHERE Plant.StatusID = ListingStatus.StatusID")
+        plantListing = cursor.fetchall()
+    #print(plantListing)
+    return render(request, "reportPage2.html", {"PlantLocation": plantListing})
+
+def displayReport3(request):
+    plants=Plant.objects.all()
+    #plants = Plant.objects.all().values('plantId','commonName','scientificName','yearLastDocumented','rankId','groupId','groupId__taxGroup','subgroupId','statusId')
+    return render(request, "reportPage3.html", {"Plant": plants})
 
